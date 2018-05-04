085b2a2659a5ff9108df5f5fdfaac47442ead819
little-scale <seb.tomczak@gmail.com>
2018-05-04 23:12:04 +0930

    Add files via upload

--- quad_probability_gate/quad_probability_gate.ino
+++ quad_probability_gate/quad_probability_gate.ino
@@ -1,17 +1,20 @@
- // Quad Probability Gate
-// by Sebastian Tomczak 7 April 2018
+// Quad Probability Gate
+// by Sebastian Tomczak 4 May 2018
 
-int gate_in = 0;
-int gate_out1 = 1;
-int gate_out2 = 2;
-int gate_out3 = 3;
-int gate_out4 = 4;
-int ledclock = 5;
-int ledcv = 6;
-int led1 = 9;
-int led2 = 10;
-int led3 = 11;
-int led4 = 12;
+#include <SoftPWM.h>
+#include <SoftPWM_timer.h>
+
+int gate_in = 1;
+int gate_out1 = 4;
+int gate_out2 = 6;
+int gate_out3 = 8;
+int gate_out4 = 10;
+int ledclock = 2;
+int ledcv = 0;
+int led1 = 3;
+int led2 = 5;
+int led3 = 7;
+int led4 = 9;
 int probability_in = A0;
 int seed_in = A5;
 int gate_flag = 0;
@@ -33,12 +36,35 @@ void setup() {
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
 
+  digitalWrite(led1, HIGH); 
+  delay(100); 
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH); 
+  delay(100); 
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH); 
+  delay(100); 
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, HIGH); 
+  delay(100); 
+  digitalWrite(led4, LOW);
+  digitalWrite(ledclock, HIGH); 
+  delay(100); 
+  digitalWrite(ledclock, LOW);
+  digitalWrite(ledcv, HIGH); 
+  delay(100); 
+  digitalWrite(ledcv, LOW);
+  SoftPWMBegin();
+  SoftPWMSet(ledcv,0);
+
   randomSeed(analogRead(seed_in));
+
+  
 }
 
 void loop() {
   data = analogRead(probability_in);
-  analogWrite(ledcv, data >> 2);
+  SoftPWMSet(ledcv, data >> 2);
 
   if (digitalRead(gate_in) == 1 && gate_flag == 0) {
     gate_flag = 1;
